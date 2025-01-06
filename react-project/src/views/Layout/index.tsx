49d80a994b4aee31928c2e94daeade7b4b7c18bf
hhh <hhh@qq.com>
2025-01-06 17:37:51 +0800

    fix:对登陆页面做优化

--- react-project/src/views/Layout/index.tsx
+++ react-project/src/views/Layout/index.tsx
@@ -25,11 +25,6 @@ const barName = [
     },
 ]
 function NavBar() {
-    // 导航栏的状态
-    // const [curBar, setCurBar] = useState('对话')
-    // const change = (name: string) => {
-    //     setCurBar(name)
-    // }
     const navigate = useNavigate()
     const switchRoute = (path:string) => {
         console.log(path);
@@ -37,10 +32,10 @@ function NavBar() {
     };
     return (
         <div className='layout'>
-            <div>
+            <div className="container">
                 <Outlet />
             </div>
-            <div className="navBars">
+            <div className="footer">
                 {/* 导航栏的名字 */}
                 <TabBar onChange={switchRoute}>
                     {barName.map((item) => (
--- react-project/src/views/Login/index.tsx
+++ react-project/src/views/Login/index.tsx
@@ -1,12 +1,27 @@
-// import { useState } from "react";
+import { useState } from "react";
 import "./login.scss"
 // import axios from 'axios';
-import { Button, Form, Input } from "antd";
+import { Button, Form, Input, message } from "antd";
 // http:192.168.43.147:7000;
 
 
 
 const Login = () => {
+  //手机号
+  const [phone, setPhone] = useState("");
+  const phoneChange = (value:string) => {
+    setPhone(value);
+  };
+  //验证码
+  const [captcha, setCaptcha] = useState("");
+  const captchaChange = (value:string) => {
+    setCaptcha(value);
+    if(value === '123456'){
+      setLogin(false)
+    }
+  };
+  // 登陆按钮状态
+  const [login, setLogin] = useState(true);
   // const [inputPhone, setInputPhone] = useState("");
   // const tologin = () => {
   //   console.log(inputPhone)
@@ -18,47 +33,65 @@ const Login = () => {
   //     }
   //   });
   // }
-  const [form] = Form.useForm();
-
-  const isPhoneValid = () => {
-    const errors = form.getFieldsError();
-
-    console.log(errors)
-    return !errors;
-  };
-
+  // 表单提交
+  const onFinish = () => {
+    if(captcha === ''){
+      message.success('发送成功,验证码为123456')
+    }else if(captcha !== '' && captcha !== '123456'){
+      message.error('验证码错误,验证码为123456')
+    }else{
+      message.success('登陆成功')
+    }
+  }
   return (
     <div className="login-background">
       <div className="login-continer">
         <div className="title">登陆体验更多精彩</div>
-        {/* <input type="text" value={inputPhone} onChange={(e) => setInputPhone(e.target.value)} />
-        <Button type="primary" shape="round" onClick={tologin}>
-          登 陆
-        </Button> */}
         <div className="form">
-          <Form form={form} >
+          <Form onFinish={onFinish} validateTrigger="onBlur" >
             <Form.Item
               name="phone"
-              rules={[{ required: true, message: '请输入正确的手机号', pattern: new RegExp(/^(13[0-9]|14[01456879]|15[0-35-9]|16[2567]|17[0-8]|18[0-9]|19[0-35-9])\d{8}$/, "g") }]}>
-              <Input className="number" placeholder="请输入手机号" />
+              rules={[
+                // 非空校验
+                {
+                  required: true,
+                  message: "请输入手机号",
+                },
+                // 手机号校验
+                {
+                  pattern: /^1[3-9]\d{9}$/,
+                  message: "请输入正确的手机号",
+                },
+              ]}>
+              <Input className="number" placeholder="请输入手机号" value={phone} onChange={(e: React.ChangeEvent<HTMLInputElement>) => phoneChange(e.target.value)}/>
             </Form.Item>
+            <Form.Item
+              rules={[{
+                required: true,
+                message: "请输入验证码",
+              }]}>
+              <Input className="number" placeholder="请输入验证码" value={captcha} onChange={(e: React.ChangeEvent<HTMLInputElement>) => captchaChange(e.target.value)}/>
+            </Form.Item>
+            <Form.Item>
+              <Button
+                type="primary"
+                className="login-captcha"
+                htmlType="submit"
+              >
+                获取验证码
+              </Button>
+            </Form.Item>
+            <Form.Item>
+              <Button
+                type="primary"
+                className="login-btn"
+                htmlType="submit"
+                disabled={login}
+              >
+                登 陆
+              </Button>
+            </Form.Item>       
           </Form>
-          {/* <Input className="number" /> */}
-          <Input className="number" placeholder="请输入验证码" />
-          <Button
-            type="primary"
-            className="login-captcha"
-          >
-            获取验证码
-          </Button>
-          <Button
-            type="primary"
-            className="login-btn"
-            // onClick={tologin}
-            disabled={!isPhoneValid()}
-          >
-            登 陆
-          </Button>
         </div>
       </div>
     </div>
